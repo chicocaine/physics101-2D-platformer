77edc0b8c68d14068d89b40d97eff616d4ed0a2f
chicocaine <eduardanthony1208@gmail.com>
2025-12-01 12:32:55 +0800

    feat: Implement key collection and next level transition
    - Added key collection functionality with animated sprites.
    - Created a new scene for keys and integrated it into the level.
    - Modified level manager to handle key counts and next level transitions.
    - Updated player script to respond to key collection and next level entry.
    - Adjusted collision layers for player and next level areas.
    - Introduced input handling for transitioning to the next level when keys are collected.

--- scripts/main.gd
+++ scripts/main.gd
@@ -8,6 +8,13 @@ var _level_manager : LevelManager
 var _gui_manager : GUIManager
 var _camera_controller : CameraController
 
+var _player_in_next_level_exit : bool
+var levels : Array[String] = [
+		"dev_test_level",
+		"dev_test_level_1"
+	]
+var current_level_idx : int
+
 func _ready() -> void:	
 	Global.main_manager = self
 	_main2D = $Main2D
@@ -18,19 +25,21 @@ func _ready() -> void:
 	_camera_controller = Global.camera_controller
 	
 	if (load_initial_level() == 0):
-		spawn_player()
+		_level_manager.spawn_player()
 	
 	_camera_controller.set_target(_player)
 	_camera_controller.follow_target = true
 	_camera_controller.cam_process_callback = Util.CamProcessCallback.PHYSICS
 	_camera_controller._set_level_size()
+	
+	_handle_signals()
 
 func _process(_delta: float) -> void:
 	pass
 
 func load_initial_level() -> int:
 	if (Global.dev_mode == Util.DevMode.TEST):
-		var s = _level_manager.load_level("dev_test_level")
+		var s = _level_manager.load_level(levels[current_level_idx])
 		if(s == 1):
 			return 1
 		print("Test Level Loaded")
@@ -43,21 +52,34 @@ func load_initial_level() -> int:
 		return 0
 	return 0
 
-func spawn_player() -> int:
-	var level = Global.current_level_2D
-	if (!level or !_player):
-		return 1
-	var player_spawnpoint = level.get_node("PlayerSpawnpoint")
-	level.add_child(_player)
-	level.move_child(_player, -1)
-	if (!player_spawnpoint):
-		return 1
-	_player.global_position = player_spawnpoint.global_position
-	return 0
+func _handle_signals() -> void:
+	_level_manager._next_level.body_entered.connect(_next_level_body_entered)
+	_level_manager._next_level.body_exited.connect(_next_level_body_exited)
+	
+	for key in (Global.current_level_2D.get_tree().get_nodes_in_group("Keys")):
+		key.key_collected.connect(_handle_key_collected)
 
-func remove_player() -> int:
-	var level = Global.current_level_2D
-	if (!level.has_node("Player")):
-		return 1
-	level.remove_child(_player)
-	return 0
+func _next_level_body_entered(body: Node2D) -> void:
+	if (body.name == "Player"):
+		_player_in_next_level_exit = true
+
+func _next_level_body_exited(body: Node2D) -> void:
+	if (body.name == "Player"):
+		_player_in_next_level_exit = false
+
+func _handle_next_level() -> void:
+	var key_count : int = _level_manager._key_count
+	var key_collected_count : int = _level_manager._key_collected_count
+	if (key_count != key_collected_count):
+		print("Denied Entry: Not enough keys")
+		return
+	self.current_level_idx += 1
+	print(self.current_level_idx)
+	_level_manager.switch_level(levels[current_level_idx])
+
+func _input(event: InputEvent) -> void:
+	if (event.is_action_pressed("up") and _player_in_next_level_exit):
+		_handle_next_level()
+
+func _handle_key_collected() -> void:
+	_level_manager._add_collected_count()
