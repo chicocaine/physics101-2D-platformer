18ee71dc84b81dc2691cbe42d95e83afb7939aaf
ketsupL <neilmorales557@gmail.com>
2025-12-07 13:34:18 +0800

    commit for merge

--- scripts/environment/pressure_plate.gd
+++ scripts/environment/pressure_plate.gd
@@ -2,22 +2,22 @@ extends Area2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 
+enum PlateColor {
+	RED = 0,
+	GREEN = 1,
+	BLUE = 2,
+	YELLOW = 3
+}
+@export var current_color: PlateColor = PlateColor.RED
 @export var target_node: Node2D
 
-const RED_FRAME_UP: int = 0
-const RED_FRAME_DOWN: int = 4
-const GREEN_FRAME_UP: int = 1
-const GREEN_FRAME_DOWN: int = 5
-const BLUE_FRAME_UP: int = 2
-const BLUE_FRAME_DOWN: int = 6
-const YELLOW_FRAME_UP: int = 3
-const YELLOW_FRAME_DOWN: int = 7
+const DOWN_OFFSET = 4
 
 signal stepped_on
 signal stepped_off
 
 func _ready():
-	sprite.frame = RED_FRAME_UP
+	sprite.frame = current_color
 	
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
@@ -25,17 +25,17 @@ func _ready():
 	if target_node:
 		if target_node.has_method("activate"):
 			stepped_on.connect(target_node.activate)
-			
 		if target_node.has_method("deactivate"):
 			stepped_off.connect(target_node.deactivate)
 
-# Mask on 5
 func _on_body_entered(_body):
 	print("Pressure plate stepped on")
 	emit_signal("stepped_on")
-	sprite.frame = RED_FRAME_DOWN
+
+	sprite.frame = current_color + DOWN_OFFSET
 
 func _on_body_exited(_body):
 	print("Pressure plate stepped off of")
 	emit_signal("stepped_off")
-	sprite.frame = RED_FRAME_UP
+
+	sprite.frame = current_color
