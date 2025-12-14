a09450cb178030865502ed68001b37f1ae88c95d
Luzeffle <165398543+Luzeffle@users.noreply.github.com>
2025-12-14 14:39:55 +0800

    Add new assets and reorganize sprites
    Huge redesign of levels

--- scripts/controllers/hud.gd
+++ scripts/controllers/hud.gd
@@ -5,7 +5,7 @@ class_name HUDController extends Control
 var _current_key_collected_count : int = 0
 
 func _ready() -> void:
-	self.label.text = "Keys Collected: %d" % [_current_key_collected_count]
+	self.label.text = "%d" % [_current_key_collected_count]
 	MessageBus.key_collected.connect(_handle_key_collected)
 	MessageBus.level_restarted.connect(reset_key_count)
 	MessageBus.level_switched.connect(reset_key_count)
@@ -15,7 +15,7 @@ func _handle_key_collected(_key: int) -> void:
 	_update_key_count()
 
 func _update_key_count() -> void:
-	self.label.text = "Keys Collected: %d" % [_current_key_collected_count]
+	self.label.text = "%d" % [_current_key_collected_count]
 
 func reset_key_count() -> void:
 	_current_key_collected_count = 0
