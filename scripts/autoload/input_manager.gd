ecaeabda6446ef4e4d3ab3140808f5254cf8fbe5
chicocaine <eduardanthony1208@gmail.com>
2025-12-14 21:47:17 +0800

    allowed input manager to run during game pause

--- scripts/autoload/input_manager.gd
+++ scripts/autoload/input_manager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+func _ready() -> void:
+	self.process_mode = Node.PROCESS_MODE_ALWAYS
+
 func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("interact")):
 		MessageBus.interact_is_pressed.emit()
